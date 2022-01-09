4fdd3cf173d982aa59ed960e3737fd844936b10c
vivian <xiaowei4508@gmail.com>
2022-01-09 16:03:14 +0800

    update

--- library/config.py
+++ library/config.py
@@ -5,10 +5,9 @@ dirpath = os.path.dirname(os.path.dirname(__file__))
 # data
 data_o_dir = os.path.join(dirpath, 'data/A榜数据')
 data_f_dir = os.path.join(dirpath, 'data/process')
+data_v_dir = os.path.join(dirpath, 'data/variable')
 
 # result
 result_a_dir = os.path.join(dirpath, 'result/analysis')
 result_p_dir = os.path.join(dirpath, 'result/predict')
-
-# model
-model_dir = os.path.join(dirpath, 'model')
+result_m_dir = os.path.join(dirpath, 'result/model')
